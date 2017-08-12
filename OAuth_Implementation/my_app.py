16104982630b4fc50e6de4a592b23a7e615bcfe1
reedjosh <reedjosh@oregonstate.edu>
2017-08-11 17:08:07 -0700

    Everything works, but the sites are sort of overly simple, the code isn't the cleanest, and there aren't enough comments...

--- OAuth_Implementation/my_app.py
+++ OAuth_Implementation/my_app.py
@@ -7,73 +7,126 @@
 #
 # Description: Contains the boat ndb model and boat handler classes.
 #
-# Use: To run, enter "dev_appserver.py ./*". Then check the porst listed in 
-# the console. Thereafter, the page shouldu be usable at that port via a 
+# Use: To run, enter "dev_appserver.py ./*". Then check the ports listed in 
+# the console. Thereafter, the Handler should be usable at that port via a 
 # browser or rest client.
 
 import os
 import json
 import jinja2
 import webapp2
-from google.appengine.api import app_identity
+import urllib
 import google.appengine.api.urlfetch as urlfetch
 
-response = urlfetch.get('http://python.org/')
-print(response.status, response.reason)
-print(len(response.content))
+
+# The following are the app's id's.
+CLIENT_ID = "976169239874-c4tghbm9r0400si67vhu6nhv5u2i75br.apps" + \
+            ".googleusercontent.com"  
+CLIENT_SECRET = "mWx1FT7jRMWEqFE89tmwk9NY" 
+
+
+# This is the url to request an oauth token from google.
+GOOGLE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+
+# This is the url to request user data once a token is obtained.
+GPLUS_URL = "https://www.googleapis.com/plus/v1/people/me"
+
+# Switching either of these allows for local or cloud hosting.
+# Local is useful for development.
+HOME_URL = "https://oauth-reedjosh.appspot.com"
+HOME_URL = "http://localhost:8080"
+REDIRECT_URI = HOME_URL + "/oauth"
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
+
+
+# OAuth Handler -> Gets user data and posts it to user_info.html page.
 class OauthHandler(webapp2.RequestHandler):
     def get(self):
-        logging.debug('The contents of the Get request are:' + repr(self.request.GET))
+        callback_state = self.request.get('state')
+        code = self.request.get('code')
+        
+        if callback_state != "SuperSecret9000":
+        	template_vars = {'error': "State var didn't match."}
+        	template = JINJA_ENVIRONMENT.get_template('500_server_error.html')
+        	self.response.write(template.render(template_vars))
+        	return
 
-# Main page handler.
-class MainPage(webapp2.RequestHandler):
+        # Setup for token request.
+        header = {'Content-Type':'application/x-www-form-urlencoded'}
+        post_vars ={'code':code,
+        	    'client_id':CLIENT_ID,
+        	    'client_secret':CLIENT_SECRET,
+        	    'redirect_uri':REDIRECT_URI,
+        	    'grant_type':'authorization_code'}
+        encoded_data = urllib.urlencode(post_vars)
+
+        # Request token.
+        result = urlfetch.fetch("https://www.googleapis.com/oauth2/v4/token/",
+                                headers=header, payload=encoded_data,
+                                method=urlfetch.POST)
+        token = json.loads(result.content)
+
+        # Google Plus URL (https required).
+        header = {'Authorization': 'Bearer ' + token['access_token']}
+        response = urlfetch.fetch(url=GPLUS_URL, headers=header)
+        
+        gplus_data = json.loads(response.content)
+    
+        template = JINJA_ENVIRONMENT.get_template('user_info.html')
+        template_vars = {'username': gplus_data['displayName'],
+                         'email': gplus_data['emails'][0]['value']}
+        self.response.write(template.render(template_vars))
+        return
+ 
+
+# Main handler.
+class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template_vars = {"title": "Welcome to ABC Inc."}
+        # Build the url to request OAuth credentials from Google
+        login = GOOGLE_URL + '?' + 'response_type=code&' + \
+                'client_id=' + CLIENT_ID +'&' + \
+                'redirect_uri=' + REDIRECT_URI + '&' + \
+                'scope=email&' + 'state=SuperSecret9000'
+        print(login)
+        template_vars = {"title": "Welcome to ABC Inc.",
+                         "login": login}
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.out.write(template.render(template_vars))
 
-class AboutPage(webapp2.RequestHandler):
+
+
+class AboutHandler(webapp2.RequestHandler):
     def get(self):
         template_vars = {"title": "About ABC Inc."}
         template = JINJA_ENVIRONMENT.get_template('about.html')
         self.response.out.write(template.render(template_vars))
 
-class ProductsPage(webapp2.RequestHandler):
+
+
+class NotFoundHandler(webapp2.RequestHandler):
     def get(self):
-        template_vars = {"title": "Products"}
-        template = JINJA_ENVIRONMENT.get_template('products.html')
+        template_vars = {"title": "404 Not Found",
+                         "url": "bad_url..."} # TODO have this post it's own url.
+        template = JINJA_ENVIRONMENT.get_template('404_page_not_found.html')
         self.response.out.write(template.render(template_vars))
 
-class ProductsPage(webapp2.RequestHandler):
-    def get(self):
-        try:
-            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-            result = urlfetch.fetch(
-                url='http://localhost:8080/submit_form',
-                method=urlfetch.GET,
-                headers=headers)
-            self.response.write(result.content)
-        except urlfetch.Error:
-            logging.exception('Caught exception fetching url')
-        template_vars = {"title": "Products"}
-        template = JINJA_ENVIRONMENT.get_template('products.html')
-        self.response.out.write(template.render(template_vars))
-            
+
+
 # Add patch as a http method.
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
+
 # Set http routes.
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/index.html', MainPage),
-                               ('/about.html', AboutPage),
-                               ('/products.html', ProductsPage),
-                               ('/oauth', OauthHandler)],
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/index.html', MainHandler),
+                               ('/about.html', AboutHandler),
+                               ('/oauth', OauthHandler),
+                               ('/.*', NotFoundHandler)],
                               debug=True)
     
